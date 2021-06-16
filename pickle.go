8eb8ecf938ce601a9b9798db4777ef2bd57b50e1
lufia <lufia@lufia.org>
2021-06-16 19:52:35 +0900

    implement Match for simple matching

--- pickle.go
+++ pickle.go
@@ -2,6 +2,8 @@
 package graphitepickletest
 
 import (
+	"fmt"
+	"strings"
 	"time"
 )
 
@@ -10,12 +12,27 @@ type Operator uint8
 
 // Operators.
 const (
-	Equal Operator = iota
-	LessThan
+	LessThan Operator = iota
 	LessEqual
 	GreaterThan
 	GreaterEqual
 )
+
+// String returns the representation of the operator.
+func (op Operator) String() string {
+	switch op {
+	case LessThan:
+		return "<"
+	case LessEqual:
+		return "<="
+	case GreaterThan:
+		return ">"
+	case GreaterEqual:
+		return ">="
+	default:
+		panic("unknown operator")
+	}
+}
 
 // Expr represents a expression.
 type Expr struct {
@@ -24,17 +41,33 @@ type Expr struct {
 }
 
 // Rule represents a rule for matching each lines in the protocol message.
+//
+// BUGS(lufia): currently Path does not support tags syntax.
 type Rule struct {
 	Required bool    // whether a rule should match to the message at least once.
 	Path     string  // dot separated path; it can be contained some wildcards (* or #).
 	Exprs    []*Expr // if Exprs is empty, that rule only checks the path exists.
 }
 
+// String returns the string representation of the rule.
+func (r *Rule) String() string {
+	exprs := make([]string, len(r.Exprs))
+	for i, e := range r.Exprs {
+		exprs[i] = fmt.Sprintf("%s%g", e.Op, e.Value)
+	}
+
+	flag := ""
+	if !r.Required {
+		flag = "~"
+	}
+	return fmt.Sprintf("%s%s:%v", flag, r.Path, strings.Join(exprs, ","))
+}
+
 // Metric represents a metric of the protocol.
 type Metric struct {
 	Path      string
-	Timestamp time.Time
 	Value     float64
+	Timestamp time.Time
 }
 
 // InvalidData contains invalid data.
@@ -47,7 +80,96 @@ type InvalidData struct {
 	Metric *Metric
 }
 
+type ruleMap struct {
+	tree map[string]*ruleMap
+
+	rules    []*Rule
+	required bool
+	used     int
+}
+
+func (m *ruleMap) isLeaf() bool {
+	return m.tree == nil
+}
+
+func (m *ruleMap) leaves() []*ruleMap {
+	var a []*ruleMap
+
+	if m.isLeaf() {
+		return nil
+	}
+	for _, v := range m.tree {
+		if v.isLeaf() {
+			a = append(a, v)
+			continue
+		}
+		a = append(a, v.leaves()...)
+	}
+	return a
+}
+
+func (m *ruleMap) addRule(p []string, r *Rule) {
+	for _, s := range p {
+		if m.tree == nil {
+			m.tree = make(map[string]*ruleMap)
+		}
+		v, ok := m.tree[s]
+		if !ok {
+			v = &ruleMap{}
+			m.tree[s] = v
+		}
+		m = v
+	}
+	m.rules = append(m.rules, r)
+	if r.Required {
+		m.required = true
+	}
+}
+
+func (m *ruleMap) lookupPath(p []string) *ruleMap {
+	for _, s := range p {
+		v, ok := m.tree[s]
+		if !ok {
+			return nil
+		}
+		m = v
+	}
+	return m
+}
+
+func makeRules(rules []*Rule) *ruleMap {
+	var m ruleMap
+	for _, r := range rules {
+		p := splitMetricName(r.Path)
+		m.addRule(p, r)
+	}
+	return &m
+}
+
+func splitMetricName(s string) []string {
+	return strings.Split(s, ".")
+}
+
 // Match checks validity of rules and metrics and returns any invalid data.
-func Match(rules []*Rule, metrics []*Metric) ([]*InvalidData, error) {
-	return nil, nil
+func Match(rules []*Rule, metrics []*Metric) []*InvalidData {
+	var results []*InvalidData
+
+	m := makeRules(rules)
+	for _, c := range metrics {
+		p := splitMetricName(c.Path)
+		r := m.lookupPath(p)
+		if r == nil || !r.isLeaf() {
+			results = append(results, &InvalidData{Metric: c})
+			continue
+		}
+		r.used++
+	}
+	for _, l := range m.leaves() {
+		if l.used == 0 { // TODO: r.required
+			for _, r := range l.rules {
+				results = append(results, &InvalidData{Rule: r})
+			}
+		}
+	}
+	return results
 }
--- pickle_test.go
+++ pickle_test.go
@@ -1,15 +1,200 @@
 package graphitepickletest
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 )
 
-func TestMatch(t *testing.T) {
-	r, err := Match([]*Rule{}, []*Metric{})
-	if err != nil {
-		t.Fatal(err)
+func TestRule_String(t *testing.T) {
+	tests := []struct {
+		name string
+		rule *Rule
+		s    string
+	}{
+		{
+			name: "optional",
+			rule: &Rule{
+				Path: "a.b.c",
+				Exprs: []*Expr{
+					{Op: LessThan, Value: 3.0},
+				},
+			},
+			s: "~a.b.c:<3",
+		},
+		{
+			name: "required",
+			rule: &Rule{
+				Required: true,
+				Path:     "a.b.c",
+				Exprs: []*Expr{
+					{Op: LessThan, Value: 3.0},
+				},
+			},
+			s: "a.b.c:<3",
+		},
+		{
+			name: "operators",
+			rule: &Rule{
+				Required: true,
+				Path:     "a.b.c",
+				Exprs: []*Expr{
+					{Op: LessThan, Value: 3.0},
+					{Op: LessEqual, Value: 2.15},
+					{Op: GreaterThan, Value: 0.0},
+					{Op: GreaterEqual, Value: -3.0},
+				},
+			},
+			s: "a.b.c:<3,<=2.15,>0,>=-3",
+		},
 	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := tt.rule.String()
+			if s != tt.s {
+				t.Errorf("%+v = %q; want %q", tt.rule, s, tt.s)
+			}
+		})
+	}
+}
+
+func TestMatchEmpty(t *testing.T) {
+	r := Match([]*Rule{}, []*Metric{})
 	if len(r) != 0 {
 		t.Errorf("should match all rules if empty rules and metrics; but found invalid data: %v", r)
 	}
+}
+
+func TestMatch_path(t *testing.T) {
+	tests := []struct {
+		name    string
+		rules   []*Rule
+		metrics []*Metric
+		want    []*InvalidData
+	}{
+		{
+			name: "simple path/success",
+			rules: []*Rule{
+				{
+					Required: true,
+					Path:     "custom.metric1.value",
+					Exprs: []*Expr{
+						{Op: LessEqual, Value: 3.0},
+					},
+				},
+			},
+			metrics: []*Metric{
+				{
+					Path:  "custom.metric1.value",
+					Value: 3.0,
+				},
+			},
+			want: nil,
+		},
+		{
+			name: "simple path/failure",
+			rules: []*Rule{
+				{
+					Required: true,
+					Path:     "custom.metric1.value",
+					Exprs: []*Expr{
+						{Op: LessEqual, Value: 3.0},
+					},
+				},
+			},
+			metrics: []*Metric{
+				{
+					Path:  "custom.metric1.value1",
+					Value: 3.0,
+				},
+				{
+					Path:  "custom.metric2.value",
+					Value: 3.0,
+				},
+				{
+					Path:  "custom.metric1",
+					Value: 3.0,
+				},
+			},
+			want: []*InvalidData{
+				{
+					Rule: &Rule{
+						Required: true,
+						Path:     "custom.metric1.value",
+						Exprs: []*Expr{
+							{Op: LessEqual, Value: 3.0},
+						},
+					},
+				},
+				{
+					Metric: &Metric{
+						Path:  "custom.metric1.value1",
+						Value: 3.0,
+					},
+				},
+				{
+					Metric: &Metric{
+						Path:  "custom.metric2.value",
+						Value: 3.0,
+					},
+				},
+				{
+					Metric: &Metric{
+						Path:  "custom.metric1",
+						Value: 3.0,
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := Match(tt.rules, tt.metrics)
+			checkResults(t, "only result", a, tt.want)
+			checkResults(t, "only expected", tt.want, a)
+		})
+	}
+}
+
+func checkResults(t *testing.T, name string, a1, a2 []*InvalidData) {
+	t.Helper()
+	if diffs := diffInvalidData(a1, a2); len(diffs) > 0 {
+		t.Run(name, func(t *testing.T) {
+			for _, d := range diffs {
+				t.Errorf("unexpected %v", d)
+			}
+		})
+	}
+}
+
+// diffInvalidData returns a slice in a part of the a1 that is *NOT* contained in a2.
+func diffInvalidData(a1, a2 []*InvalidData) []*InvalidData {
+	var x []*InvalidData
+	for _, v1 := range a1 {
+		var found bool
+		for _, v2 := range a2 {
+			if reflect.DeepEqual(v1, v2) {
+				found = true
+			}
+		}
+		if !found {
+			x = append(x, v1)
+		}
+	}
+	return x
+}
+
+func (p *InvalidData) String() string {
+	s := ""
+	if p.Rule != nil {
+		s += fmt.Sprintf("rule={%v}", p.Rule)
+	}
+	if p.Metric != nil {
+		if s != "" {
+			s += " "
+		}
+		s += fmt.Sprintf("value={%s=%g}", p.Metric.Path, p.Metric.Value)
+	}
+	return s
 }
