8e0b7166bd2392dc6fbfd5d32e8a19fe6ef68edb
lufia <lufia@lufia.org>
2021-06-17 12:13:55 +0900

    add docs

--- cmd/graphite-metric-test/main.go
+++ cmd/graphite-metric-test/main.go
@@ -1,3 +1,29 @@
+// graphite-metric-test is a verifier for Graphite Plaintext Protocol format.
+//
+// Usage:
+//
+//	graphite-metric-test [-f rule] [file ...]
+//
+// The rule option is a file contains rules with metric path patterns and metric value ranges.
+// graphite-metric-test reads the rules, reads metrics (from stdin by default),
+// verify them and reports mismatches.
+//
+// The Rules
+//
+// The rule described in the rule file each lines is a pair of metric path pattern and value range.
+//
+//	// comment
+//	local.random.diceroll	>0, <=6	 // v > 0 && v <= 6
+//	local.thermal.*.temp	<=100000 // wildcard (* or #) matches any stem in the path
+//	~local.network.tx.bytes	>0 // path starting with ~ is optional
+//	local.uptime // no range; it checks path existence but the value is not checked
+//
+// If you want to check metrics with OR condition, you can put multiple lines with same path pattern.
+//
+//	local.signal.level		>=0, <2
+//	local.signal.level		>=3, <5
+//
+// Finally, the operators are '<=', '<', '>=' and '>'.
 package main
 
 import (
